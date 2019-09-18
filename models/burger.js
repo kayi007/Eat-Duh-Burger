634ddf55e80553160e53d6b67fc60caf4fbce6b1
Kathleen Leung <kathleenleungky@gmail.com>
2019-09-18 16:25:08 -0700

    completed orm.js and created controllers folder

--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,7 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var burger = {
-    all: function(cb){
-        orm.selectAll
-    }
-};
+var burger = {};
 
 // Export the database functions for the controller (burgers_controller.js).
 module.exports = burger;
